8befd3b4b0b1cd26359ce5c38a7d95807482836c
SyrupMap <163905244+syrupmap@users.noreply.github.com>
2026-07-26 22:08:48 -0500

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,12 @@
+window._config = {
+  cognito: {
+    userPoolId: "YOUR_USER_POOL_ID",
+    userPoolClientId: "YOUR_APP_CLIENT_ID",
+    region: "YOUR_REGION"
+  },
+  api: {
+    invokeUrl: "YOUR_API_GATEWAY_URL"
+  }
+};
+
+//PLACEHOLDERS BECAUSE ORIGINAL CONTAINS INFORMATION ON MY AWS DEPLOYMENT
